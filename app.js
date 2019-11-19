78939785c68a57835ab6bca7e67977a542149d37
dunice <you@example.com>
2019-11-19 15:21:26 +0300

    one part

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
-// function Accumulator(startingValue) {
-//     this.value = startingValue;
+let todos = [];
+let todoValue;
+let id = 0;
 
-//     this.read = function() {
-//         this.value += +prompt('Сколько нужно добавить?', 0);
-//     };
-
-// }
-
-// let accumulator = new Accumulator(1);
-// accumulator.read();
-// accumulator.read();
-// console.log(accumulator.value);
-
-
-// let fruits = ['Apples', 'Orange', 'Pear'];
-// alert ( fruits.pop());
-// alert(fruits);
-
-// let todoValue = document.getElementsByClassName('newTodo');
-// let val = todoValue.value;
-// console.log(val);
-
-add.onclick = function() {
-  let todo = document.getElementById('newTodo').value;
-  document.getElementById('listTodo').innerHTML = `${todo}`;
+function TodoItem(completed, remove, value, id) {
+  this.completed = completed;
+  this.remove = remove;
+  this.value = value;
+  this.id = id; 
 }
+
+function clearTodoList() {
+  document.getElementById('newTodo').value = "";
+}
+
+function todoList() {
+  listTodo.innerHTML = '';
+  for (let todo of todos) {
+    $('#listTodo').append(`<input type="text" id="newTodo" class="newTodoList" value="${todo.value}">
+                           <div id="remove"><i class="fa fa-trash"></i></div>`);
+  };
+}
+
+$('#add').on('click', function() {
+  todoValue = document.getElementById('newTodo').value;
+  if (todoValue !== '') {
+    addList(todoValue);
+  } 
+})
+
+function addList(todoValue) {
+  id = ++id;
+  let todo = new TodoItem(false, false, todoValue, id);
+    todos.push(todo);
+    todoList();
+    clearTodoList();
+    console.log(todo);
+}
+
+$('.fa-trash:before').on('click', function() {
+  alert(1);
+});
+
+function remove() {
+  todos.forEach( function(todo, i, todos) {
+    if(todo.remove == true) {
+      delete todos[i];
+    }
+  });
+}
+
+
+
+
+
+
