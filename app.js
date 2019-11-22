e7e9ac63c28e4c74f349d8a5ce21c2171d10ed0f
dunice <you@example.com>
2019-11-22 12:22:05 +0300

    edit todo

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ let id = 0;
 let idLi;
 let counter;
 let activeTodos;
+let toggle = true;
 
 function createTodoItem(completed, remove, value, id) {
   this.completed = completed;
   this.remove = remove;
   this.value = value;
-  this.id = id; 
+  this.id = id;
 }
 
 function clearInput() {
@@ -22,31 +23,32 @@ function counterTodo(counterTodos) {
   return counterDOM.innerHTML = `<p>${counter} items left</p>`;
 }
 
-function createTodoList() {
-  listTodo.innerHTML = '';
-  for (let todo of todos) {
-    $('#listTodo').append(
-      `<li id="${todo.id}">
-      <i class="fa fa-check" id="checkTodo"></i>
-      <input type="text"  class="newTodoList"  id="rename" value="${todo.value}">
-      <i class="fa fa-trash"></i>
-      </li>`
-    );
+function createTodoList(arr = todos) {
+  let str = ``;
+  for (let todo of arr) {
+    str = str +
+      `<li id="${todo.id}" ${todo.completed && 'class="activeCheck"'}>
+      <input type="checkbox" class="checkTodo"/>
+      <input type="text"  class="newTodoList"  id="edit" value="${todo.value}">
+      <i class="delete-todo">x</i>
+      </li>`;
   };
+  // fa fa-trash
+  $('#listTodo').html(str)
   counterTodos = todos;
   counterTodo(counterTodos);
 }
 
-$('#add').on('click', function() {
+$('#add').on('click', function () {
   todoValue = document.getElementById('newTodo').value;
   if (todoValue !== '') {
     addTodo();
-  } 
+  }
 })
 
-$('#newTodo').on('keypress', function(e) {
+$('#newTodo').on('keypress', function (e) {
   todoValue = document.getElementById('newTodo').value;
-  if(e.keyCode == 13 && todoValue !== '') {
+  if (e.keyCode == 13 && todoValue !== '') {
     addTodo();
   }
 })
@@ -54,119 +56,138 @@ $('#newTodo').on('keypress', function(e) {
 function addTodo() {
   id = ++id;
   let todo = new createTodoItem(false, false, todoValue, id);
-    todos.push(todo);
-    createTodoList();
-    clearInput();
+  todos.push(todo);
+  createTodoList();
+  clearInput();
 }
 
+function checkAll() {
+  if (toggle == false) {
+    todos.forEach(function (todo) {
+      todo.completed = false;
+    });
+  } else {
+    todos.forEach(function (todo) {
+      todo.completed = true;
+    });
+  }
+}
+
+$(document).on('click', '.checkAll', function () {
+  toggle = !toggle;
+  checkAll();
+  createTodoList();
+});
+
+// !!!!!!!!
 $('body').on('click', '.fa-trash', function () {
   idLi = $(this).parent().attr(`id`);
-  todos.forEach(function(todo) {
-    if(todo.id == idLi) {
+  todos.forEach(function (todo) {
+    if (todo.id == idLi) {
       todo.remove = true;
     }
   });
-  let removeTodos = todos.filter(function(todo) {
-    if(todo.remove == false) {
+  let removeTodos = todos.filter(function (todo) {
+    if (todo.remove == false) {
       return true;
-    }; 
+    };
   });
   todos = removeTodos;
   createTodoList();
 });
 
-$('#allTodo').on('click', function() {
+$('#allTodo').on('click', function () {
   createTodoList();
 });
 
-$('#clearTodo').on('click', function() {
-  todos = [];
+$('#clearCompletedTodo').on('click', function () {
+  let clearCompletedTodos = todos.filter(function (todo) {
+    if (todo.completed == true) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+  todos = clearCompletedTodos;
   createTodoList();
 });
 
-$('#activeTodo').on('click', function() {
+$('#activeTodo').on('click', function () {
   showActiveTodo();
 });
 function showActiveTodo() {
-  activeTodos = todos.filter(function(todo) {
-    if(todo.completed == false) {
+  activeTodos = todos.filter(function (todo) {
+    if (todo.completed == false) {
       return true;
     }
   });
-  listTodo.innerHTML = '';
-  for (let todo of activeTodos) {
-    $('#listTodo').append(
-      `<li id="${todo.id}">
-      <i class="fa fa-check" id="checkTodo"></i>
-      <input type="text"  class="newTodoList" id="rename" value="${todo.value}">
-      <i class="fa fa-trash"></i>
-      </li>`
-    );
-  };
   counterTodos = activeTodos;
-  counterTodo(counterTodos);  
+  counterTodo(counterTodos);
 }
 
-function renameTodo() {
-  $('#newTodo').on('keypress', function(e) {
+function editTodo() {
+  console.log('funktion')
+  $('#edit').on('keypress', function (e) {
+    console.log('enter')
     todoValue = document.getElementById('newTodo').value;
-    if(e.keyCode == 13 && todoValue !== '') {
-      todos.forEach(function(todo) {
-        if(todo.id == idLi) {
+    if (e.keyCode == 13 && todoValue !== '') {
+      todos.forEach(function (todo) {
+        if (todo.id == idLi) {
           todo.value = renameValue;
         }
       });
-    }  
+    }
   })
 }
-
-
-$(document).on('dblclick', `#rename`,function() {
+$(document).on('dblclick', `#edit`, function () {
   // todoValue = document.getElementById('newTodo').value;
   idLi = $(this).parent().attr(`id`);
   let renameValue = this.value;
-  
-  console.log(todos);
+  editTodo();
+  // console.log(todos);
 });
 
-$('#completedTodo').on('click', function() {
+$('#completedTodo').on('click', function () {
   showCompletedTodo();
 });
 function showCompletedTodo() {
-  let completedTodos = todos.filter(function(todo) {
-    if(todo.completed == true) {
+  let completedTodos = todos.filter(function (todo) {
+    if (todo.completed == true) {
       return true;
     }
   });
-  listTodo.innerHTML = '';
-  for (let todo of completedTodos) {
-    $('#listTodo').append(
-      `<li id="${todo.id}">
-      <label><i class="fa fa-check" id="checkTodo"></i></label>
-      <input type="text"  class="newTodoList" value="${todo.value}">
-      <i class="fa fa-trash"></i>
-      </li>`
-    );
-  };
+  createTodoList(completedTodos);
+  // listTodo.innerHTML = '';
+  // for (let todo of completedTodos) {
+  //   $('#listTodo').append(
+  //     `<li id="${todo.id}">
+  //     <label><i class="fa fa-check" id="checkTodo"></i></label>
+  //     <input type="text"  class="newTodoList" id="edit" value="${todo.value}">
+  //     <i class="fa fa-trash"></i>
+  //     </li>`
+  //   );
+  // };
   counterTodos = completedTodos;
-  counterTodo(completedTodos);  
+  counterTodo(completedTodos);
 }
 
-function checkInput() {
-  
-}
-
-$('body').on('click', '.fa-check', function () {
+$('body').on('change', '.checkTodo', function () {
   idLi = $(this).parent().attr(`id`);
-  todos.forEach(function(todo) {
-    if(todo.id == idLi) {
+  let kek = $(this).prop(`checked`);
+  let kek2 = $(this).is(':checked');
+  console.log(`kek`, kek)
+  console.log(`kek2`, kek2)
+
+  todos.forEach(function (todo) {
+    if (todo.id == idLi) {
       todo.completed = true;
     }
   });
-  $(this).toggleClass('activeCheck');
-  $(this).parent().addClass('activeText');
-  console.log(this);
-  console.log(idLi);
+  createTodoList();
+  // $(this).toggleClass('activeCheck');
+  // $(this).parent().addClass('activeText');
+  // console.log(this);
+  // console.log(idLi);
 });
 
 // $('#something').on('click', function() {
